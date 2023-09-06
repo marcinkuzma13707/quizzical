47bfc7704f68be4a850d78e5c20cfcef4267266e
marcin13707 <marcinkuzma13707@gmail.com>
2023-09-06 20:58:14 +0200

    finished implementation

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,8 @@ import React, {useState, useEffect} from "react"
 
 function App() {
   const [started, setStarted] = useState(false)
-  const [finished, setFinished] = useState(false)
   const [count, setCount] = useState(0)
-  const [correctCount, setCorrectCount] = useState(0)
+  const [correct, setCorrect] = useState(0)
   const [checked, setChecked] = useState(false)
   const [questions, setQuestions] = useState([])
 
@@ -16,7 +15,7 @@ const shuffleArray = (arr) => arr.sort(() => Math.random() - 0.5)
 
 useEffect(() =>{
   async function getQuestions(){
-    const res = await fetch('https://opentdb.com/api.php?amount=10')
+    const res = await fetch('https://opentdb.com/api.php?amount=5&encode=base64')
     const data = await res.json()
     const tmp = []
     data.results.forEach(q =>{
@@ -40,12 +39,51 @@ const questionsSection = questions ? questions.map(q =>{
     key={q.id}
     question={q}
     id={q.id}
+    handleClickAnswer={handleClickAnswer}
     />
   )
 }) : []
 
   function startQuiz(){
     setStarted(x => !x)
+  }
+
+  function handleEnd(checked){
+    if(checked){
+      setCount(count => count + 1)
+      setChecked(false)
+      return
+    }
+
+    let allSelected = true;
+    questions.forEach(q =>{
+      if(q.selected === null){
+        allSelected = false
+        return
+      }
+    })
+
+    if(!allSelected)return
+
+    setQuestions(prevQuestions => prevQuestions.map(q => {
+      return {...q, checked:true}
+    }))
+
+    setChecked(true)
+    let correct = 0;
+    questions.forEach(q =>{
+      if(q.selected === q.correctAnswer) correct++;
+    })
+    
+    setCorrect(correct)
+  }
+
+  function handleClickAnswer(id,answer){
+    setQuestions(questions => questions.map(q => {
+      return q.id !== id
+             ? q
+             : {...q, selected:answer}
+    }))
   }
   return (
     <div className="main-container">
@@ -54,8 +92,10 @@ const questionsSection = questions ? questions.map(q =>{
             started ? 
             <div className="start-content-container">
               {questionsSection}
-              <div>
-                <button>{finished ? "Play again" : "Check answers"}</button>
+              <div className="end-container">
+                {checked && <span className="score"> You scored {correct} / {questions.length} correct answers</span>}
+                <button className="check" onClick={() => handleEnd(checked)}>{checked ? "Play again" : "Check answers"}
+                </button>
               </div>
             </div>
             : <Menu 
--- src/Question.jsx
+++ src/Question.jsx
@@ -2,8 +2,40 @@ import React from 'react'
 import {nanoid} from "nanoid"
 
 function Question(props){
+
+    const answers = props.question.answers.map(a =>{
+        var decodedAnswer = atob(a)
+        let id = null;
+        if(props.question.checked){
+            if(props.question.correctAnswer === a){
+                id = 'correct'
+            }
+            else if(props.question.selected === a){
+             id = 'incorrect'
+            }
+            else{
+                id = 'not-selected'
+            }
+        }
+        return (
+            <button 
+             id={id}
+             key={a} 
+             className={a===props.question.selected ? 'answer selected' : 'answer'}
+             onClick={() => handleClick(a)}>{decodedAnswer}</button>
+        )
+    })
+
+    function handleClick(a){
+        if(props.question.checked) return
+        props.handleClickAnswer(props.id,a)
+    }
+
     return(
-        <div>{props.question.question}</div>
+        <div className="question-container">
+            <h3 className="question-title">{atob(props.question.question)}</h3>
+            {answers}
+            </div>
     )
 }
 
